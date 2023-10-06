6601f27fcd3be96537fc45ba400c8438384e896e
Breno Hildebrand <hello@brenohildebrand.com>
2023-10-06 05:17:01 -0300

    hey there

--- protocol.h
+++ protocol.h
@@ -1,30 +1,16 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   client.c                                           :+:      :+:    :+:   */
+/*   protocol.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: bhildebr <bhildebr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/10/06 01:49:57 by bhildebr          #+#    #+#             */
-/*   Updated: 2023/10/06 01:49:57 by bhildebr         ###   ########.fr       */
+/*   Created: 2023/10/06 05:15:34 by bhildebr          #+#    #+#             */
+/*   Updated: 2023/10/06 05:15:34 by bhildebr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#ifndef PROTOCOL_H
+#define PROTOCOL_H
 
-int	main(int argc, char *argv[])
-{
-	if (argc != 3)
-	{
-		printf("Usage: ./client <server-pid> <string>\n");
-		return (0);
-	}
-
-	// validate pid
-	// validate string
-
-	// for each bit of each byte/char of the string, send the SIGUSR1 if
-	// it is on, or the SIGUSR2 otherwise. 
-
-	return (0);
-}
+#endif
--- server.c
+++ server.c
@@ -10,7 +10,15 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+
 int	main(int argc, char *argv[])
 {
+	pid_t pid;
+
+	pid = getpid();
+	printf("%d\n", pid);
 	return (0);
 }
